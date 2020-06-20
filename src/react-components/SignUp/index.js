e34a34f25410b7c430a5902bafaf4f43f95b0cbd
Ibrahim Mahjoub <imahjoub@live.ca>
2020-06-20 11:42:26 -0400

    cleaned up SignUp component

--- src/react-components/SignUp/index.js
+++ src/react-components/SignUp/index.js
@@ -2,53 +2,62 @@ import React from "react";
 
 import "./styles.css";
 
+// import DoctorSignUp from "./../DoctorSignUp";
+
 /* Component for user registration */
 class SignUp extends React.Component {
 	
+	// list of all possible status values
+	status = ['referral', 'doctor', 'secretary', 'patient'];
+	
 	constructor(props) {
 		super(props);
+		
 		this.state = {
-			status: 'signup',
+			statusIndex: 0,
 		};
 	}
 	
 	render() {
 		return (
 			<div className="SignUp">
-				{(() => {
-					switch (this.state.status) {
-						case 'signup':
-							return (
-								<div>
-									<p>Enter Referral Code.</p>
-									<button onClick={ () => this.changeState('doctor') }>Doctor</button>
-								</div>);
-						case 'doctor':
-							return (
-								<div>
-									<p>Doctor Signup.</p>
-									<button onClick={ () => this.changeState('secretary') }>Secretary</button>
-								</div>);
-						case 'secretary':
-							return (	
-								<div>
-									<p>Secretary Signup</p>
-									<button onClick={ () => this.changeState('patient') }>Patient</button>
-								</div>);
-						case 'patient':
-							return (
-								<div>
-									<p>Patient Signup.</p>
-									<button onClick={ () => this.changeState('signup') }>SignUp</button>
-								</div>);
-					}
-				})()}
+				{this.getSignUpForm(this.state.statusIndex)}
+				{/* for testing */}
+				<button onClick={ () => this.nextStatus(this.state.statusIndex) }>{this.status[this.state.statusIndex]}</button>
 			</div>
 		);
 	}
 	
-	changeState (value) {
-		this.setState({ status: value });
+	// returns the appropriate signup form based on the statusIndex
+	getSignUpForm(index) {
+		return(
+			<React.Fragment>
+				{
+					{
+						// referral
+						0: <p>Enter Referral Code.</p>,
+						// doctor
+						// 1: <DoctorSignUp appComponent={this.props.appComponent} />,
+						1: <p>Doctor SignUp</p>,
+						// secretary
+						2: <p>Secretary Signup</p>,
+						// patient
+						3: <p>Patient Signup.</p>,
+					}[index]
+				}
+			</React.Fragment>
+		)
+	}
+	
+	// sets the statusIndex value
+	setStatus (value) {
+		this.setState({ statusIndex: value });
+	}
+	
+	// sets statusIndex to the next status value
+	// for testing
+	nextStatus (statusIndex) {
+		this.setStatus((this.state.statusIndex + 1) % 4);
 	}
 }
 
