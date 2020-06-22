47574a8f372656c9a34d8c6e887eadedd7aa4318
Ibrahim Mahjoub <imahjoub@live.ca>
2020-06-22 08:09:01 -0400

    removed header from app component

--- src/App.js
+++ src/App.js
@@ -20,7 +20,6 @@ class App extends React.Component {
   render(){
     return (
       <div className="App">
-	    <Header />
         <BrowserRouter>
           <Switch>
             <Route exact path='/' render={() => 
