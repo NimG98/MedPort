2169d9c0ad94eb346b417682470c57ff9e502762
Nimrah Gill <nimrahg@hotmail.com>
2020-07-03 19:24:44 -0400

    write app feature descriptions

--- src/react-components/Home/index.js
+++ src/react-components/Home/index.js
@@ -24,28 +24,30 @@ class Home extends React.Component {
         </Row>
         <Row className="featureDisplay">
           <Col span={8}>
-            <Card title="Feature 1" bordered={false} className="roundedCardLeft">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
-            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis 
-            nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore 
-            eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
-            sunt in culpa qui officia deserunt mollit anim id est laborum.
+            <Card title="View your medical test reports online" bordered={false} className="roundedCardLeft">
+              After a patient gets a medical test done, their test report will be uploaded online 
+              in one convenient place for them and their family doctor to review.
+              This removes the need for you to have to go into the doctor’s office to receive your test results.
+              You can also upload your previously received test results from other doctors or specialists for further analysis.
             </Card>
           </Col>
           <Col span={8}>
-            <Card title="Feature 2" bordered={false}>
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium 
-            doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore 
-            veritatis et quasi architecto beatae vitae dicta sunt explicabo. 
-            Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut 
-            fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
+            <Card title="Receive feedback along with test reports" bordered={false}>
+              A family doctor can upload test results with attached notes to discuss with you.
+              By commenting on a thread under the test result, a doctor and patient can easily discuss test results with each other.
+              You can reply to the doctor's feedback, ask any questions, and raise any concerns.
+              In case you forget what your doctor recommended; no worries!
+              You can easily refer back to the documented record of the discussion.
+              This keeps conversations organized as they are attached to the specific test result in discussion.
             </Card>
           </Col>
           <Col span={8}>
-            <Card title="Feature 3" bordered={false} className="roundedCardRight">
-            Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam 
-            nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
+            <Card title="Doctor-Patient communication made easy" bordered={false} className="roundedCardRight">
+              Further discussion between the doctor and the patient can be facilitated on
+              the website by requesting an in-person or over-the-phone appointment. You or your doctor can submit a request
+              to set a time to call each other or visit the doctor's office. After a doctor looks at a test result and thinks
+              you need a new follow-up test conducted, they can easily send you a request to get the test done and provide more information
+              inside the request itself!
             </Card>
           </Col>
         </Row>
