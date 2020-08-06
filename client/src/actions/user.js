ef95c03d9d57e749514f043fe29d60fd6cfe5f37
Nimrah <nimrahg@hotmail.com>
2020-08-05 21:54:24 -0400

    added api routes file

--- client/src/actions/user.js
+++ client/src/actions/user.js
@@ -1,8 +1,10 @@
 // Functions to help with user actions.
 
+import { ApiRoutes } from "../constants/apiRoutes";
+
 /* Gets the type of the user (patient/doctor/admin) */
 export const getUserType = (username, callback, component) => {
-    const url = "/api/users/userType/" + username
+    const url = ApiRoutes.getUserType + username;
     var userType = null;
     
     return fetch(url)
@@ -30,9 +32,7 @@ export const getUserType = (username, callback, component) => {
 
 // A function to check if a user is logged in on the session cookie
 export const readCookie = (app) => {
-    const url = "/api/users/check-session";
-
-    fetch(url)
+    fetch(ApiRoutes.checkSession)
         .then(res => {
             if (res.status === 200) {
                 return res.json();
@@ -61,7 +61,7 @@ export const updateLoginForm = (loginComp, field) => {
 // A function to send a POST request with the user to be logged in
 export const login = (loginComp, app) => {
     // Create our request constructor with all the parameters we need
-    const request = new Request("/api/users/login", {
+    const request = new Request(ApiRoutes.login, {
         method: "post",
         body: JSON.stringify(loginComp.state),
         headers: {
@@ -89,7 +89,7 @@ export const login = (loginComp, app) => {
 
 // A function to send a GET request to logout the current user
 export const logout = (app) => {
-    const url = "/api/users/logout";
+    const url = ApiRoutes.logout;
 
     fetch(url)
         .then(res => {
@@ -106,7 +106,7 @@ export const logout = (app) => {
 /* Returns the profile info of the loggedInUser or a different user by username */
 export const getUserProfileInfo = (username, callback) => { 
     // get all of the profile info for the loggedInUser
-    var url = "/api/profile/";
+    var url = ApiRoutes.profile;
 
     // get general profile info for a different user (firstname, lastname, email)
     if(username){
---
+++ client/src/constants/apiRoutes.js
@@ -0,0 +1,10 @@
+// export const baseUrl = 'http://localhost:5000'; // LEAVE THIS UNCOMMENTED
+export const baseUrl = '';
+export const ApiRoutes = {
+    login: baseUrl + '/api/users/login',
+    getUserType: baseUrl  +'/api/users/userType/',
+    checkSession: baseUrl + '/api/users/check-session',
+    logout: baseUrl + '/api/users/logout',
+    createUser: baseUrl + "/api/users",
+    profile: baseUrl + "/api/profile/"
+}
