311cf9d197af08a21492d8cd63bb199ac18a7694
Nimrah <nimrahg@hotmail.com>
2020-08-15 06:48:43 -0400

    add call to newsapi.org for canadian health news, added ui to dashboard

---
+++ client/src/actions/healthNews.js
@@ -0,0 +1,35 @@
+// Functions to help with health news.
+
+
+/* Get top health news articles in Canada */
+export const getTopHealthNews = (component) => {
+    const url = "http://newsapi.org/v2/top-headlines?country=ca&category=health&apiKey=64a619f995b14d4cad1e409027ef7f4b"
+
+    return fetch(url)
+        .then(res => {
+            if (res.status === 200) {
+                return res.json();
+            }
+        })
+        .then(newsArticlesJson => {
+            if (newsArticlesJson) {
+                const condensedNewsArticleArray = [];
+                // const condensedNewsArticleArray = newsArticlesJson.articles.slice(0, 6);
+                for(var article of newsArticlesJson.articles) {
+                    if(condensedNewsArticleArray.length === 6) {
+                        break;
+                    }
+                    if(article.urlToImage){
+                        condensedNewsArticleArray.push(article);
+                    }
+                }
+                if(component){
+                    component.setState({ newsArticleArray: condensedNewsArticleArray });
+                }
+                return condensedNewsArticleArray;
+            }
+        })
+        .catch(error => {
+            console.log(error);
+        });
+}
--- client/src/react-components/Dashboard/index.js
+++ client/src/react-components/Dashboard/index.js
@@ -1,80 +1,85 @@
 import React from "react";
-
+import { withRouter } from "react-router";
 
 import "./styles.css";
 import 'antd/dist/antd.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, Row, Col} from 'react-bootstrap'
-//import $ from 'jquery';
-//import Popper from 'popper.js';
-//import 'bootstrap/dist/js/bootstrap.bundle.min';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import { Container, Row, Col} from 'react-bootstrap'
+import { Card, Row }  from "antd";
+import backgroundPic from "./static/dashboard-picture.png"
 
-import { withRouter } from "react-router";
+// import '@trendmicro/react-sidenav/dist/react-sidenav.css';
 
-import '@trendmicro/react-sidenav/dist/react-sidenav.css';
-import 'antd/dist/antd.css';
-//import { Sidenav } from 'rsuite';
-//import 'rsuite/lib/styles/index.less';
 import Header from './../Header';
 import NavBar from './../NavBar';
+import { getUserProfileInfo } from "../../actions/user";
+import { getTopHealthNews } from "../../actions/healthNews";
+
+const { Meta } = Card;
+
 /* Component for the Dashboard */
 class Dashboard extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.props.history.push("/dashboard");
-  }
+    constructor(props) {
+        super(props);
+        this.props.history.push("/dashboard");
+        this.state = {
+            name: null
+        }
+        this.getNewsArticles = this.getNewsArticles.bind(this);
+        this.onClickArticle = this.onClickArticle.bind(this);
+    }
 
-  render() {
-    // To make sure no one just visits http://localhost:3000/dashboard
-    // without logging in first
-    // if (document.cookie.indexOf("LoggedInSession=Valid") === -1) {
-    //     window.location.href = "/";
-    // }
-    return (
-        <div>
-            <Header appComponent={this.props.appComponent}/>
-            <NavBar appComponent={this.props.appComponent}/>
+    async componentDidMount() {
+        const loggedInUserGeneralProfile = (await getUserProfileInfo()).generalProfile;
+        const name = loggedInUserGeneralProfile.firstName + " " + loggedInUserGeneralProfile.lastName;
+        this.setState({ name: name })
+        await getTopHealthNews(this);
+        this.getNewsArticles();
+    }
 
-            
+    onClickArticle = (articleUrl) => {
+        const win = window.open(articleUrl, '_blank');
+    }
 
-            
-            <div className="container2">
-                <Row>
-                    
-                    <Col className="col-md size-1">
-                        <h2 className="RH2">News:</h2>
-                        <h5>- news story</h5>
-                        <h5>- another news story</h5>
-                        <h5>- another news story</h5>
-                        <h5>- another news story</h5>
-                    </Col>
-                    <br/>
-                    <Col className="col-md size-1">
-                        <h2 className="RH2">Journals:</h2>
-                        <h5>- Journal</h5>
-                        <h5>- another Journal</h5>
-                        <h5>- another Journal</h5>
-                        
-                    </Col>
-                </Row>
-                <Row className="size-2">
-                <h2 className="RH2">Health News/Latest advancements</h2>
-                    
-                        <h5>- Health news story</h5>
-                        <h5>- another health news story</h5>
-                        <h5>- Latest Advancement</h5>
-                        <h5>- another Latest Advancement</h5>
-                </Row>
+    getNewsArticles = () => {
+        var newsArticleCards = [];
+        for(var newsArticle of this.state.newsArticleArray) {
+            newsArticleCards.push(
+                <Card className="newsArticleCard" hoverable 
+                    onClick={() => this.onClickArticle(newsArticle.url)}
+                    cover={<img alt="newsArticleImage" src={newsArticle.urlToImage} />}
+                >
+                    <Meta title={newsArticle.title} description={newsArticle.description} />
+                </Card>
+            )
+        }
+        var newsArticleRows = [];
+        newsArticleRows.push(<Row className="newsArticleRow"> {newsArticleCards.slice(0,3)} </Row>)
+        // newsArticleRows.push(<Row className="newsArticleRow"> {newsArticleCards.slice(3,6)} </Row>)
+        console.log(newsArticleRows);
+        this.setState({ newsArticleRowData: newsArticleRows})
+    }
+
+    render() {
+        return (
+            <div>
+                <Header appComponent={this.props.appComponent}/>
+                <NavBar appComponent={this.props.appComponent}/>
+
+                <div className="container2">
+                    <div className="dashboardWelcomeContainer">
+                        <Row gutter>
+                            <img className="dashboardBackgroundImage" alt="online-report-image" src={backgroundPic}/>
+                        </Row>
+                        <h1 className="dashboardWelcomeMessage">Welcome to MedPort, {this.state.name}!</h1>
+                    </div>
+                    <h2 className="newsSectionTitle">Health News Articles</h2>
+                    {this.state.newsArticleRowData}
+                </div>
             </div>
-            
-           
-
-            
-        </div>
-    );
-  }
-
+        );
+    }
 }
 
 export default withRouter(Dashboard);
